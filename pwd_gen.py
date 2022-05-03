94a49972fe773ce06ace6608d7c582598e7e4669
cryptoshepherd <lellothegreat@protonmail.ch>
2022-05-03 23:59:51 +0200

    first commit

---
+++ pwd_gen.py
@@ -0,0 +1,16 @@
+import secrets
+import string
+
+
+def password_gen(password_length):
+
+    characters = string.ascii_letters + string.digits + string.punctuation
+
+    secure_password = ''.join(secrets.choice(characters) for i in range(password_length))
+
+    print(secure_password)
+
+    #return secure_password
+
+password_gen(20)
+
